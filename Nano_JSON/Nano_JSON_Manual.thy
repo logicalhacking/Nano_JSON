c507e20ae11f47fc5e689564254f8bb1874a8f75
Achim D. Brucker <adbrucker@0x5f.org>
2022-07-03 22:42:04 +0100

    First draft of manual.

--- Nano_JSON/Nano_JSON_Manual.thy
+++ Nano_JSON/Nano_JSON_Manual.thy
@@ -33,8 +33,46 @@ theory
 imports 
   Nano_JSON_Query
 begin
+
+subsection\<open>Isabelle/HOL\<close>
 text\<open>
-  This is the main entry point into the @{session "Nano_JSON"} session for users of this 
-  AFP entry.
+  On the Isabelle/HOL level, we provide three new commands for working with JSON formatted
+  data.
+\<close>
+
+paragraph\<open>@{command "JSON"}.\<close> text\<open> For defining JSON data within HOL terms,
+we provide the following command:
+  \<^rail>\<open> @@{command "JSON"} json_data \<close>
+where \emph{json-data} is a cartouche containing the JSON data.
+\<close>
+
+paragraph\<open>@{command "JSON_file"}.\<close> text\<open> For reading JSON data from an external 
+file, we provide the command 
+ \<^rail>\<open> @@{command "JSON_file"} json_filename 'defining' json_def \<close>
+where \emph{json-filename} is the path (file name) of the JSON file that should be read
+and  \emph{json-def} is the name the HOL definition representing the JSON data is bound to.\<close>
+
+paragraph\<open>@{command "JSON_export"}.\<close> text\<open> For serializing (exporting) JSON 
+encoded data, we provide the command
+ \<^rail>\<open> @@{command "JSON_export"} json_def  ('file' json_filename)? \<close>
+where \emph{json-def} is the definition of the JSON data that should be exported. Optionally
+a file name (\emph{json-filename}) can be provided. If a file name is provided, it is used
+for storing the serialized data in Isabelle's virtual file system. If no file name is provided, 
+the serialized data is printed in Isabelle's output window.
+\<close>
+
+paragraph\<open>Configuration.\<close> text \<open>We provide three configuration attributes:
+\<^item> The attribute @{attribute "JSON_num_type"} (default @{type "int"} allows for configuring the 
+  HOL-type used representing JSON numerals.
+\<^item> The attribute @{attribute "JSON_string_type"}  (default @{type "string"}) allows for configuring 
+  the HOL-type used representing JSON string.
+\<^item> The attribute @{attribute "JSON_verbose"} (default: false) allows for enabling warnings during the
+  JSON processing.
+\<close>
+
+subsection\<open>Isabelle/ML\<close>
+text\<open>
+  We refer users who want to use or extend Nano JSON on the Isabelle/ML level to the ML signatures
+  shown in the implementation chapter (\autoref{ch:implementation}). 
 \<close>
 end
